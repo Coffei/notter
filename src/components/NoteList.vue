01f324e630ba6084128f017d31a424dcb9f9e3e6
Jonas Trantina <jonas.trantina@altworx.com>
2025-02-14 10:51:08 +0100

    Add trash icon to the remove-note button

--- src/components/NoteList.vue
+++ src/components/NoteList.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
 import { useRoute, useRouter } from 'vue-router';
 import { notes, addNote } from '../store'
 
@@ -32,8 +34,8 @@ const removeNote = (index: number) => {
             <div v-else class="font-semibold text-slate-400">Untitled</div>
             <div class="text-slate-400 text-sm overflow-hidden text-nowrap text-ellipsis ">{{ note.text }}</div>
             <button
-                class="absolute top-2 right-2 p-1 border border-orange-600 bg-orange-800 rounded cursor-pointer hidden group-hover/note:block"
-                @click.stop.prevent="removeNote(index)">Remove</button>
+                class="absolute top-2 right-2 px-2 py-1 border border-orange-600 bg-orange-800 rounded cursor-pointer hidden group-hover/note:block"
+                @click.stop.prevent="removeNote(index)"><FontAwesomeIcon :icon="faTrashCan" /></button>
         </RouterLink>
     </div>
 </template>
