2351b2c897f999a9b489c7a46b576b8c411e6af5
Jonas Trantina <jonas.trantina@altworx.com>
2025-02-14 10:29:10 +0100

    Add base path so we can host it

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,10 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
+const base = process.env.NODE_ENV === 'production' ? '/notter/' : '/'
 // https://vite.dev/config/
 export default defineConfig({
+  base,
   plugins: [
     vue(),
     vueDevTools(),
