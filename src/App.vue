4c8177a0932c4647dba7816a805bcefe92fd7361
Jonas Trantina <jonas.trantina@altworx.com>
2025-02-14 10:45:38 +0100

    Add link to Github
    Add FontAwesome icons and a smal link to Github repo.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { RouterView } from 'vue-router'
 </script>
 
 <template>
     <div class="h-full flex flex-col">
-        <header class="h-16 px-4 border-b-2 border-lime-800 flex items-center bg-slate-800">
+        <header class="h-16 px-4 border-b-2 border-lime-800 flex justify-between items-center bg-slate-800">
             <h1 class="text-2xl font-medium text-white">Notter</h1>
+            <a href="https://github.com/Coffei/notter" target="_blank" title="See the source code on GitHub!"><FontAwesomeIcon :icon="faGithub"/></a>
         </header>
 
         <div class="flex-1">
