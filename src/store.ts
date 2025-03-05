b8b3cd1a2f7cb4f28473167f1ff2c84bf7cab5a8
Jonas Trantina <jonas.trantina@altworx.com>
2025-03-05 16:20:19 +0100

    Add created and updated times to notes
    Existing notes will work fine without a time. It will be added once
    modified.

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,6 @@
 import { reactive, watch } from "vue"
 
-export interface Note { id: string; title: string; text: string; }
+export interface Note { id: string; title: string; text: string; createdAt: number, updatedAt: number }
 
 // initialize notes from local storage
 const storedNotes: Note[] = JSON.parse(localStorage.getItem('notes') || '[]')
@@ -12,7 +12,8 @@ watch([notes], () => {
 })
 
 export const addNote = () => {
-    const newNote: Note = { id: crypto.randomUUID(), title: '', text: '' }
+    const now = new Date().getTime()
+    const newNote: Note = { id: crypto.randomUUID(), title: '', text: '', createdAt: now, updatedAt: now }
     notes.push(newNote)
     return newNote
 }
