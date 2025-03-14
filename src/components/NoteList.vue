5766233c965017d586d5c7486a9a88af811c717a
Jonas Trantina <jonas.trantina@altworx.com>
2025-03-14 13:48:21 +0100

    Add note ordering
    Use can order the notes by few criteria.

--- src/components/NoteList.vue
+++ src/components/NoteList.vue
@@ -2,6 +2,9 @@
 import { useRoute, useRouter } from 'vue-router';
 import { notes, addNote } from '../store'
 import NoteListItem from './NoteListItem.vue';
+import NoteOrder from './NoteOrder.vue';
+import { computed, ref } from 'vue';
+import type { Note } from '../store';
 
 const router = useRouter()
 const route = useRoute()
@@ -19,6 +22,24 @@ const addNoteAndSwitch = () => {
     const note = addNote()
     router.push(`/notes/${note.id}`)
 }
+const order = ref('update_desc')
+
+type DisplayNote = { note: Note, index: number }
+const displayNotes = computed(() => {
+    let sortFn: (a: DisplayNote, b: DisplayNote) => number = (a, b) => ((b.note.updatedAt || 0) - (a.note.updatedAt || 0))
+    if (order.value == 'update_asc') {
+        sortFn = (a, b) => ((a.note.updatedAt || 0) - (b.note.updatedAt || 0))
+    } else if (order.value == 'create_desc') {
+        sortFn = (a, b) => ((b.note.createdAt || 0) - (a.note.createdAt || 0))
+    } else if (order.value == 'create_asc') {
+        sortFn = (a, b) => ((a.note.createdAt || 0) - (b.note.createdAt || 0))
+    } else if (order.value == 'title_desc') {
+        sortFn = (a, b) => (b.note.title.localeCompare(a.note.title))
+    } else if (order.value == 'title_asc') {
+        sortFn = (a, b) => (a.note.title.localeCompare(b.note.title))
+    }
+    return notes.map((note, index) => ({ note, index })).sort(sortFn)
+})
 </script>
 
 <template>
@@ -26,6 +47,10 @@ const addNoteAndSwitch = () => {
         <button class="w-full bg-slate-900 hover:bg-lime-950 p-4 cursor-pointer" @click="addNoteAndSwitch">
             Create new note
         </button>
-        <NoteListItem v-for="(note, index) in notes" :note="note" :index="index" @remove="removeNote" />
+        <div class="flex justify-between">
+            <div />
+            <NoteOrder :selected="order" @change="(selected) => order = selected" />
+        </div>
+        <NoteListItem v-for="{ note, index } in displayNotes" :note="note" :index="index" @remove="removeNote" />
     </div>
 </template>
---
+++ src/components/NoteOrder.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{ selected: string }>()
+const orders = [
+    { value: "update_desc", name: "Last updated at" },
+    { value: "update_asc", name: "First updated at" },
+    { value: "create_desc", name: "Last created at" },
+    { value: "create_asc", name: "First created at" },
+    { value: "title_asc", name: "Title ascending" },
+    { value: "title_desc", name: "Title descending" }
+]
+const emit = defineEmits<{ change: [order: string] }>()
+const change = (e: Event) => {
+    if (e.currentTarget) {
+        emit('change', (e.currentTarget as HTMLSelectElement).value)
+    }
+}
+</script>
+<template>
+    <div class="px-2 py-1 text-sm underline decoration-dashed">
+        <select @change="change" class="appearance-none cursor-pointer border-b-1 border-dashed">
+            <option v-for="item in orders" :selected="selected == item.value" :value="item.value">{{ item.name }}
+            </option>
+        </select>
+    </div>
+</template>
